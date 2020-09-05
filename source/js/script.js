9448e33d7dad7c52b7d655d3c7ad07f303f1329a
Seperkopian Jakarta <awetsemoga38@gmail.com>
2020-09-05 14:22:35 +0700

    add new another search modal

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,40 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
 /*声明三个自定义js方法*/
 /*不区分大小写的判断包含， 用于搜索文章标题过滤文章*/
 jQuery.expr[':'].contains = function (a, i, m) {
